2221de59360aa3d0902d670279b38b873211735e
bkwaan <bkwan15@my.bcit.ca>
2020-06-23 23:31:25 -0700

    fixed routes and being able to pull from database

--- api/Pokemon.js
+++ api/Pokemon.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
-
+const Pokemons = require('../models/kantoregion');
 
 
 router.get('/', (req, res) => {
-    console.log("request getting called");
-    res.json({'msg': 'HELLO'});
+    Pokemons.find({}, function(err, docs) {
+        if (!err) { 
+            console.log(docs);
+        }
+        else {
+            throw err;
+        }
+    });
+    res.send({'fata' : 'hi'});
 });
 
 module.exports = router;
---
+++ models/kantoregion.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const KantoRegionSchema = new Schema({},
+    {collection: 'KantoRegion'},
+    {strict:false}
+);
+
+module.exports = KantoRegion = mongoose.model('KantoRegion',KantoRegionSchema);
