97d16d9bb974b67746be0f073d60102279069cd4
bkwaan <bkwan15@my.bcit.ca>
2020-07-02 17:06:33 -0700

    added bcrypt to hash password

--- api/User.js
+++ api/User.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const Users = require("../models/users");
+const bcrypt = require("bcrypt");
+const rounds = 10;
+
 
 router.post('/', (req,res) => {
-    const {username, email,password} = req.body;
+    var {username, email,password} = req.body;
+    let salt = bcrypt.genSaltSync(rounds);
+    password = bcrypt.hashSync(password,salt);
+    console.log(password);
     var user = new Users({
         username,
         email,
