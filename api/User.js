4b7c7d175a05be3f3456144d21696d6dd206e814
bkwaan <bkwan15@my.bcit.ca>
2020-07-02 22:44:45 -0700

    added login route

--- api/User.js
+++ api/User.js
@@ -1,29 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const Users = require("../models/users");
 const bcrypt = require("bcrypt");
 const rounds = 10;
 
+router.post("/Signup", (req, res) => {
+  var { username, email, password } = req.body;
+  let salt = bcrypt.genSaltSync(rounds);
+  password = bcrypt.hashSync(password, salt);
+  var user = new Users({
+    username,
+    email,
+    password,
+  });
 
-router.post('/', (req,res) => {
-    var {username, email,password} = req.body;
-    let salt = bcrypt.genSaltSync(rounds);
-    password = bcrypt.hashSync(password,salt);
-    console.log(password);
-    var user = new Users({
-        username,
-        email,
-        password,
+  user.save()
+    .then((data) => {
+      res.send({success:true, message: 'User saved'})
     })
-
-    user.save()
-    .then(item => {
-        res.send("item saved")
-    }).catch(err => {
-       res.send(err);
-    })
-
+    .catch((err) => {
+      res.send(err);
+    });
 });
 
+router.post("/Login", (req, res) => {
+  var { email, password } = req.body;
+  Users.find({ email: email })
+    .then((data) => {
+      if (data.length > 0) {
+        if (bcrypt.compareSync(password, data[0].password)) {
+          res.json({ success: true, message: "Logged in" });
+        } else {
+            res.json({status:404, success: false, message: 'Incorrect Password'});
+        }
+      } else {
+          res.json({status: 404, success: false, message: 'Email is not registered'});
+      }
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
 
 module.exports = router;
