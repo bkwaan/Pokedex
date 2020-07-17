5e48a155c9a2f81a7f88aa3d466b243fe4398179
bkwaan <bkwan15@my.bcit.ca>
2020-07-16 20:48:50 -0700

    forgot password function implemented on server side

--- api/User.js
+++ api/User.js
@@ -3,31 +3,100 @@ const router = express.Router();
 const Users = require("../models/users");
 const bcrypt = require("bcrypt");
 const rounds = 10;
+const nodemailer = require("nodemailer");
 
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  secure: false,
+  port: 25, //SMTP port
+  auth: {
+    user: "pepeinccs@gmail.com",
+    pass: "Comp4945",
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+router.get("/:email", (req, res) => {
+  var email = req.param.email;
+  Users.find({ Email: email }).then((data) => {
+    if (data.length > 0) {
+      let emailSend = {
+        from: "Pokedex <pepeincss@gmail.com>",
+        to: email,
+        text: "TESTING",
+      };
+
+      transporter.sendMail(emailSend, (err, info) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(info.response);
+        }
+      });
+      res.send({
+        success: true,
+        message: "Sent reset email!",
+      });
+    } else {
+      res.send({
+        success: false,
+        message: "Email doesn't exist",
+      });
+    }
+  });
+});
+
+router.post("/forgotPassword", (req, res) => {
+  var email = req.body.email;
+  var password = req.body.password;
+  let salt = bcrypt.genSaltSync(rounds);
+  password = bcrypt.hashSync(password, salt);
+  Users.updateOne(
+    { Email: email },
+    {
+      Password: password,
+    }
+  )
+    .then((data) => {
+      res.send({
+        success: true,
+        message: "Password Updated!",
+      });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+// Signup users
 router.post("/Signup", (req, res) => {
   var { Username, Email, Password } = req.body;
   let salt = bcrypt.genSaltSync(rounds);
   Password = bcrypt.hashSync(Password, salt);
 
-  Users.find({ email: Email })
+  Users.find({ Email: Email })
     .then((data) => {
       if (data.length > 0) {
-        res.send({ 
-          success: false, 
-          message: "Email already exists" });
+        res.send({
+          success: false,
+          message: "Email already exists",
+        });
       } else {
         var user = new Users({
           Username,
           Email,
           Password,
         });
-      
+
         user
           .save()
           .then((data) => {
-            res.send({ 
-              success: true, 
-              message: "User saved" });
+            res.send({
+              success: true,
+              message: "User saved",
+            });
           })
           .catch((err) => {
             res.send(err);
@@ -37,22 +106,20 @@ router.post("/Signup", (req, res) => {
     .catch((err) => {
       res.send(err);
     });
-
- 
 });
 
 router.post("/Login", (req, res) => {
   var { Username, Password } = req.body;
-  console.log(Username)
-  console.log(Password)
-  Users.find({ email: Username })
+  console.log(Username);
+  console.log(Password);
+  Users.find({ Email: Username })
     .then((data) => {
       if (data.length > 0) {
         if (bcrypt.compareSync(Password, data[0].password)) {
           console.log("LOGGED IN BABY");
-          res.send({ 
-            success: true, 
-            message: "Logged in" 
+          res.send({
+            success: true,
+            message: "Logged in",
           });
         } else {
           res.send({
