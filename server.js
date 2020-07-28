15833f5a20c183e6b6ac69ec02bb9fc3457c174d
bkwaan <bkwan15@my.bcit.ca>
2020-07-28 16:05:09 -0700

    fixed deployment

--- server.js
+++ server.js
@@ -16,15 +16,19 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log("Server started on port " + PORT));
 
-const api = require("./api/Index");
+const api = require("./api");
 app.use("/api", api);
 
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname = "client/build/index.html"));
   });
 }
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
