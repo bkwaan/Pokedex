538cb5007c74fdff7fa2c10c00f4cbeda4d3a931
EO37 <pun.edwinto@gmail.com>
2020-07-29 14:57:06 +0800

    edited: added logout function to HomePage

--- client/src/HomePage/Home.js
+++ client/src/HomePage/Home.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import "./Home.css"
 import PokemonInfo from "./PokemonInfo.js"
-import {Redirect} from "react-router-dom"
+import { Redirect } from "react-router-dom"
 import pokemons from "./pokemon.json"
 import Login from "../Login/Login.js"
-import { Navbar, Nav, ListGroup, Container, Row, Col, Form, Button , ProgressBar } from 'react-bootstrap'
+import { Navbar, Nav, ListGroup, Container, Row, Col, Form, Button, ProgressBar } from 'react-bootstrap'
 import { header } from 'express-validator';
 import Fade from 'react-reveal/Fade';
 import Rotate from 'react-reveal/Rotate';
@@ -15,8 +15,8 @@ class Home extends Component {
         super(props);
         this.state = {
             types: ['Fire', 'Water', 'Grass', 'Electric', 'Psychic', 'Steel', 'Normal',
-            'Fairy', 'Dark', 'Flying', 'Ghost', 'Poison', 'Ice', 'Ground', 
-            'Rock', 'Dragon', 'Fighting', 'Bug'],
+                'Fairy', 'Dark', 'Flying', 'Ghost', 'Poison', 'Ice', 'Ground',
+                'Rock', 'Dragon', 'Fighting', 'Bug'],
             typeSelected: [],
             search: {
                 searchWord: ""
@@ -35,16 +35,20 @@ class Home extends Component {
                 }
             },
             redirectTOLogin: false,
-            logedIn: (localStorage.getItem("login")=="true"),
+            logedIn: (localStorage.getItem("login") == "true"),
             LOGIN: "LOGIN",
             LOGOUT: "LOGOUT"
         }
     }
 
+    Logout = () => {
+        localStorage.clear();
+        this.RedirectLoginPage();
+    }
 
     RedirectLoginPage() {
         localStorage.setItem("login", "false");
-        this.setState({redirectTOLogin: true});
+        this.setState({ redirectTOLogin: true });
     }
 
     // componentDidMount() {
@@ -119,13 +123,13 @@ class Home extends Component {
         fetch('http://localhost:5000/api/Pokemon/all', {
             method: 'GET'
         })
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
 
     }
 
@@ -139,15 +143,15 @@ class Home extends Component {
                     typeAlreadySelected = true;
                 }
             })
-        } 
+        }
         if (!typeAlreadySelected) {
             if (newTypeSelected.length === 2) {
                 newTypeSelected.shift();
-            } 
+            }
             newTypeSelected.push(typeName.typeName);
 
         }
-        this.setState({typeSelected: newTypeSelected}, console.log(this.state));
+        this.setState({ typeSelected: newTypeSelected }, console.log(this.state));
         //console.log(newTypeSelected);
         // if (typeName.typeName !== this.state.typeSelected) {
         //     console.log("different type");
@@ -168,18 +172,18 @@ class Home extends Component {
         console.log(this.state.typeSelected.length);
         this.FilterPoke();
         this.HideTypeList();
-        
+
     }
 
     FilterPoke = () => {
         const container = document.getElementsByClassName('row')[1];
         let pokemons;
-        if (this.state.search.searchWord === ""){
+        if (this.state.search.searchWord === "") {
             pokemons = container.getElementsByClassName("Pokemon--Home")
         } else {
             pokemons = container.getElementsByClassName('Pokemon--Home withSearch');
         }
-        
+
         if (pokemons.length < 1) {
             return;
         }
@@ -195,45 +199,45 @@ class Home extends Component {
                     for (let x = 0; x < typeList.length; x++) {
                         if (typeList[x].textContent === typeName) {
                             matches[y] = true;
-                            break;  
+                            break;
                         }
                     }
-                   
+
                 });
                 for (let x = 0; x < matches.length; x++) {
                     match = match && matches[x];
-                }   
+                }
             } else {
                 match = false;
-            }          
-            
+            }
+
             if (!match) {
                 pokemons[i].parentNode.style.display = "none";
                 pokemons[i].classList.remove("withFilter");
-           } else {
+            } else {
                 pokemons[i].parentNode.style.display = "block";
                 pokemons[i].classList.add("withFilter");
-           }   
-           
+            }
+
         }
     }
 
     ResetFilter = () => {
         const container = document.getElementsByClassName('row')[1];
         let pokemons;
-        if (this.state.search.searchWord === ""){
+        if (this.state.search.searchWord === "") {
             pokemons = container.getElementsByClassName("Pokemon--Home")
         } else {
             pokemons = container.getElementsByClassName('Pokemon--Home withSearch');
         }
-        
+
 
         for (let i = 0; i < pokemons.length; i++) {
             pokemons[i].classList.remove("withFilter");
             pokemons[i].parentNode.style.display = "block";
         }
 
-        if(this.state.search.searchWord !== "") {
+        if (this.state.search.searchWord !== "") {
             this.searchPokemon();
         }
     }
@@ -241,7 +245,7 @@ class Home extends Component {
     RemoveSelectedType = (i) => {
         const newSelectedType = this.state.typeSelected;
         newSelectedType.splice(i, 1);
-        this.setState({typeSelected: newSelectedType});
+        this.setState({ typeSelected: newSelectedType });
         if (this.state.typeSelected.length > 0) {
             this.FilterPoke();
         } else {
@@ -259,64 +263,64 @@ class Home extends Component {
     }
 
     getSearchKeyWord = (event) => {
-        const {search} = this.state;
+        const { search } = this.state;
         search["searchWord"] = event.target.value;
-        this.setState({search}, console.log(this.state));
+        this.setState({ search }, console.log(this.state));
         this.searchPokemon();
     }
-    
+
     searchPokemon = () => {
         let container = document.getElementsByClassName('row')[1];
         let childz;
-        
-        console.log("state: "+ this.state.typeSelected.length);
 
-        if (this.state.typeSelected.length > 0){
+        console.log("state: " + this.state.typeSelected.length);
+
+        if (this.state.typeSelected.length > 0) {
             childz = container.getElementsByClassName('Pokemon--Home withFilter');
 
         } else {
             childz = container.getElementsByClassName("Pokemon--Home");
         }
-        
+
         if (childz.length < 1) {
             return;
         }
 
-        for(var i = 0; i < childz.length; i++) {
-            if(!childz[i].id.includes(this.state.search.searchWord)){
-                childz[i].parentNode.style.display="none";
+        for (var i = 0; i < childz.length; i++) {
+            if (!childz[i].id.includes(this.state.search.searchWord)) {
+                childz[i].parentNode.style.display = "none";
                 childz[i].classList.remove("withSearch");
             } else {
-                childz[i].parentNode.style.display="block";
+                childz[i].parentNode.style.display = "block";
                 if (this.state.search.searchWord === "") {
                     childz[i].classList.remove("withSearch");
                 } else {
                     childz[i].classList.add("withSearch");
-                }  
+                }
             }
         }
-        
-        if (this.state.typeSelected.length > 0){
+
+        if (this.state.typeSelected.length > 0) {
             this.FilterPoke();
         }
-    
-        
-        
+
+
+
     }
 
     render() {
         let testingPokemonList = [];
-        if (this.state.redirectTOLogin){
-            return <Redirect to="/Login"/>
+        if (this.state.redirectTOLogin) {
+            return <Redirect to="/Login" />
         }
         console.log(this.state.logedIn);
 
         return (
             <div className="Container--Home" >
-                <div className="main--Home" onClick={(  ) => this.HideTypeList()}>
+                <div className="main--Home" onClick={() => this.HideTypeList()}>
                     <Row className="NavBar--Home">
                         <Col xs={2} sm={2} lg={2} xl={2} >
-                            <img size = "sm" src="./images/openMenu.svg" className="MenuIcon--Home" onClick={() => this.ShowTypeList()} />
+                            <img size="sm" src="./images/openMenu.svg" className="MenuIcon--Home" onClick={() => this.ShowTypeList()} />
                         </Col>
                         <Col xs={8} sm={8} lg={8} xl={8}>
                             <Form.Row>
@@ -326,21 +330,24 @@ class Home extends Component {
                             </Form.Row>
                         </Col>
                         <Col xs={2} sm={2} lg={2} xl={2}>
-        <Button variant="outline-info" size="large" onClick={()=> this.RedirectLoginPage()}>{this.state.logedIn ? this.state.LOGOUT : this.state.LOGIN}</Button>
+                            {this.state.logedIn ?
+                                <Button variant="outline-info" size="large" onClick={() => this.Logout()}> {this.state.LOGOUT}</Button> :
+                                <Button variant="outline-info" size="large" onClick={() => this.RedirectLoginPage()}>{this.state.LOGIN}</Button>
+                            }
                         </Col>
                     </Row>
                     <div className="NavBarContainer-Home"></div>
                     <Container>
                         {this.state.typeSelected.length > 0 ?
-                        <div className="typeSelected--TypeFilter">
-                            {this.state.typeSelected.map((type, i) => (
-                                <div className={"typeSelected--Home " + type} onMouseEnter={() => this.DisplayIcon(i)} onMouseLeave={() => this.HideIcon(i)}>
-                                    <p>{type}</p>
-                                    <img src="./images/closeBtn.svg" className={"closeIcon-typeSelected--Home"} onClick={() => this.RemoveSelectedType(i)}/>
-                                </div>
-                            ))}
+                            <div className="typeSelected--TypeFilter">
+                                {this.state.typeSelected.map((type, i) => (
+                                    <div className={"typeSelected--Home " + type} onMouseEnter={() => this.DisplayIcon(i)} onMouseLeave={() => this.HideIcon(i)}>
+                                        <p>{type}</p>
+                                        <img src="./images/closeBtn.svg" className={"closeIcon-typeSelected--Home"} onClick={() => this.RemoveSelectedType(i)} />
+                                    </div>
+                                ))}
 
-                        </div> : null}
+                            </div> : null}
                         <Row>
                             {testingPokemonList}
                             {/* {this.state.pokemons.map((pokemon) => { */}
@@ -356,14 +363,14 @@ class Home extends Component {
                                 return (
                                     <Col xs={6} sm={4} md={3} lg={2} >
                                         <Fade left>
-                                        {/* <div className="Pokemon--Home" id = {pokemon.name.english} onClick={() => this.OpenModal(pokemon.id, pokeID)}> */}
-                                        <div className="Pokemon--Home" id = {pokemon.name.english} onClick={() => this.OpenModal(pokeID, pokemon)}>
-                                            <img src={"/images/" + pokeID + ".png"} className="pokemonThumbnail--Home" />
-                                            <p>{pokemon.name.english}</p>
-                                            <ul className="typeList--Pokemons">
-                                                {pokemon.type.map((data) => (<li>{data}</li>))}
-                                            </ul>
-                                        </div>
+                                            {/* <div className="Pokemon--Home" id = {pokemon.name.english} onClick={() => this.OpenModal(pokemon.id, pokeID)}> */}
+                                            <div className="Pokemon--Home" id={pokemon.name.english} onClick={() => this.OpenModal(pokeID, pokemon)}>
+                                                <img src={"/images/" + pokeID + ".png"} className="pokemonThumbnail--Home" />
+                                                <p>{pokemon.name.english}</p>
+                                                <ul className="typeList--Pokemons">
+                                                    {pokemon.type.map((data) => (<li>{data}</li>))}
+                                                </ul>
+                                            </div>
                                         </Fade>
                                     </Col>)
                             })}
@@ -379,7 +386,7 @@ class Home extends Component {
                     </div>
                     <div className="TypeListContainer">
                         {this.state.types.map((typeName) => (
-                            <btn id="TypeContainer" className={typeName} onClick={()=>this.TypeFilter({typeName})}>{typeName}</btn>
+                            <btn id="TypeContainer" className={typeName} onClick={() => this.TypeFilter({ typeName })}>{typeName}</btn>
                         ))}
                     </div>
                     {/* <ListGroup>
@@ -389,7 +396,7 @@ class Home extends Component {
                     </ListGroup> */}
                 </div>
                 <Rotate>
-                <PokemonInfo pokemonInfo={this.state.pokemonInfo} CloseModal={() => this.CloseModal()} />
+                    <PokemonInfo pokemonInfo={this.state.pokemonInfo} CloseModal={() => this.CloseModal()} />
                 </Rotate>
 
                 {/* <button onClick={() => this.TestRequest()}> testing</button> */}
