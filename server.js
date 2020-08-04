a64bf3aec097497d61d2c97658c7c527358a6496
bkwaan <bkwan15@my.bcit.ca>
2020-08-04 02:47:48 -0700

    changed server.js

--- server.js
+++ server.js
@@ -22,9 +22,6 @@ app.use("/api", api);
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use('*', express.static(path.join(__dirname, "client", "build")))
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
 }
